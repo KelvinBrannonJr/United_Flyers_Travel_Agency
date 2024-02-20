b2b19ad8c41f1dcb1033c4973d071f6a9614c2ea
Kelvin <Kelzjr56@gmail.com>
2024-02-19 23:09:39 -0500

    Added header declaration for main and while loop for base UI

--- United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.cpp
+++ United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
+#include "United_Flyers_Travel_Agency.h"
+
+// Utility UI function checks user input to quit while loop in main
+bool runUI() {
+	if (runUI_User_Choice == 'Q' or runUI_User_Choice == 'q') {
+		isRunning = false;
+	}
+	return isRunning;
+}
 
 int main()
-{
-	std::cout << std::setfill('*') << std::setw(10) << "Hi";
+{	
+	// Title header, implement <iomanip> for formatting
+	std::cout << "Welcome to United Flyers" << std::endl;
+	std::cout << "" << std::endl;
+
+	isRunning = true;
+	while (isRunning == true) {
+		std::cout << "Select an option from below:" << std::endl;
+		std::cout << "Q: Quit" << std::endl;
+
+		std::cin >> runUI_User_Choice;
+		runUI();
+	}
+
+
 }
---
+++ United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.h
@@ -0,0 +1,6 @@
+#pragma once
+
+char runUI_User_Choice;
+bool isRunning = false;
+
+bool runUI();
