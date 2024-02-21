6a1bd79ef6f7ec0a0326eb00a1991c74645c8393
Kelvin <Kelzjr56@gmail.com>
2024-02-20 22:47:01 -0500

    Finished menuSelection function switch logic

--- United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.cpp
+++ United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.cpp
@@ -12,20 +12,45 @@ void displayMenu() {
 	std::cout << "B: Purchase and Book Flight" << std::endl;
 	std::cout << "V: View Confirmation details" << std::endl;
 	std::cout << "Q: Quit" << std::endl;
+	std::cout << std::endl;
+
 }
 
 // Utility function to handle the user menu selection
 void menuSelection(char choice) {
 	switch (user_choice) {
+		case 'P':
+		case 'p':
+			std::cout << user_choice << ": Nice, you would like to plan a trip!" << std::endl;
+			break;
+
+		case 'E':
+		case 'e':
+			std::cout << user_choice << ": You would like a price estimate of a for a trip." << std::endl;
+			break;
+
+		case 'B':
+		case 'b':
+			std::cout << user_choice << ": You would like to purchase a ticket and book the flight." << std::endl;
+			break;
+
+		case 'V':
+		case 'v':
+			std::cout << user_choice << ": View your confirmation details." << std::endl;
+			break;
+
 		case 'Q':
 		case 'q':
-			std::cout << "Quitting Application..Goodbye" << std::endl;
+			std::cout << user_choice << ": Quitting Application..Goodbye" << std::endl;
 			quitUI();
 			break;
 
 		default:
+			std::cout << user_choice << ": Sorry that was an invalid choice.." << std::endl;
 			break;
 	}
+	std::cout << std::endl;
+
 }
 
 // Utility UI function checks user input to quit while loop in main
@@ -47,8 +72,10 @@ int main()
 	isRunning = true;
 	while (isRunning == true) {
 		displayMenu();
+
 		std::cin >> user_choice;
 		menuSelection(user_choice);
+
 	}
 
 
