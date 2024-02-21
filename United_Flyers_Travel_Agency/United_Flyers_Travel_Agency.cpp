194445d44c97ed165cc9904736286adbeb96f617
Kelvin <Kelzjr56@gmail.com>
2024-02-20 23:39:10 -0500

    User data variables created for plan a trip function, linked function call in switch statement

--- United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.cpp
+++ United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.cpp
@@ -1,66 +1,6 @@
 #include <iostream>
+#include <string>
 #include "United_Flyers_Travel_Agency.h"
-
-
-// TASK: Finish implementing rest of menu options for menuSelection() - 02/19/2024
-
-// Utility UI display menu
-void displayMenu() {
-	std::cout << "Select an option from below:" << std::endl;
-	std::cout << "P: Plan a Trip" << std::endl;
-	std::cout << "E: Estimate Cost" << std::endl;
-	std::cout << "B: Purchase and Book Flight" << std::endl;
-	std::cout << "V: View Confirmation details" << std::endl;
-	std::cout << "Q: Quit" << std::endl;
-	std::cout << std::endl;
-
-}
-
-// Utility function to handle the user menu selection
-void menuSelection(char choice) {
-	switch (user_choice) {
-		case 'P':
-		case 'p':
-			std::cout << user_choice << ": Nice, you would like to plan a trip!" << std::endl;
-			break;
-
-		case 'E':
-		case 'e':
-			std::cout << user_choice << ": You would like a price estimate of a for a trip." << std::endl;
-			break;
-
-		case 'B':
-		case 'b':
-			std::cout << user_choice << ": You would like to purchase a ticket and book the flight." << std::endl;
-			break;
-
-		case 'V':
-		case 'v':
-			std::cout << user_choice << ": View your confirmation details." << std::endl;
-			break;
-
-		case 'Q':
-		case 'q':
-			std::cout << user_choice << ": Quitting Application..Goodbye" << std::endl;
-			quitUI();
-			break;
-
-		default:
-			std::cout << user_choice << ": Sorry that was an invalid choice.." << std::endl;
-			break;
-	}
-	std::cout << std::endl;
-
-}
-
-// Utility UI function checks user input to quit while loop in main
-bool quitUI() {
-	if (user_choice == 'Q' or user_choice == 'q') {
-		isRunning = false;
-	}
-	return isRunning;
-}
-
 
 
 int main()
@@ -80,3 +20,66 @@ int main()
 
 
 }
+
+// Feature function for planning a trip
+void planTrip() {
+	std::cout << "Lets start by getting your name: " << std::endl;
+}
+
+// Utility UI display menu
+void displayMenu() {
+	std::cout << "Select an option from below:" << std::endl;
+	std::cout << "P: Plan a Trip" << std::endl;
+	std::cout << "E: Estimate Cost" << std::endl;
+	std::cout << "B: Purchase and Book Flight" << std::endl;
+	std::cout << "V: View Confirmation details" << std::endl;
+	std::cout << "Q: Quit" << std::endl;
+	std::cout << std::endl;
+
+}
+
+// Utility function to handle the user menu selection
+void menuSelection(char choice) {
+	switch (user_choice) {
+	case 'P':
+	case 'p':
+		std::cout << user_choice << ": Nice, you would like to plan a trip!" << std::endl;
+		planTrip();
+		break;
+
+	case 'E':
+	case 'e':
+		std::cout << user_choice << ": You would like a price estimate of a for a trip." << std::endl;
+		break;
+
+	case 'B':
+	case 'b':
+		std::cout << user_choice << ": You would like to purchase a ticket and book the flight." << std::endl;
+		break;
+
+	case 'V':
+	case 'v':
+		std::cout << user_choice << ": View your confirmation details." << std::endl;
+		break;
+
+	case 'Q':
+	case 'q':
+		std::cout << user_choice << ": Quitting Application..Goodbye" << std::endl;
+		quitUI();
+		break;
+
+	default:
+		std::cout << user_choice << ": Sorry that was an invalid choice.." << std::endl;
+		break;
+	}
+	std::cout << std::endl;
+}
+
+// Utility UI function checks user input to quit while loop in main
+bool quitUI() {
+	if (user_choice == 'Q' or user_choice == 'q') {
+		isRunning = false;
+	}
+	return isRunning;
+}
+
--- United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.h
+++ United_Flyers_Travel_Agency/United_Flyers_Travel_Agency.h
@@ -1,8 +1,25 @@
-#pragma once
+#ifndef UNITED_FLYERS_TRAVEL_AGENCY_H
+#define UNITED_FLYERS_TRAVEL_AGENCY_H
+
+// Variables
+std::string first_name;
+std::string last_name;
+std::string flight_from;
+std::string flight_to;
 
 char user_choice;
+
 bool isRunning = false;
 
+// Main Function
+bool quitUI();
+
+
+// Feature Functionsvo
+void planTrip();
+
+// Utility healper functions
 void displayMenu();
 void menuSelection(char choice);
-bool quitUI();
+
+#endif
