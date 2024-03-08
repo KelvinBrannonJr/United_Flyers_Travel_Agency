dd540f0f5de1516d9f8cc87f936600bf31e8bb58
Kelvin <Kelzjr56@gmail.com>
2024-03-07 23:34:30 -0500

    Created Booking class and set desired objectives

---
+++ United_Flyers_Travel_Agency/Booking.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*
+	1: Allow customers to book a flight, can inherit logic from Estimate Costfor ease of implementation.
+	2: Provide payment gateway, possibly from an external helper class.
+	3: Finalized payment details
+*/
+
+class Booking {
+
+
+
+};
