2fcd48e4b3fffab5cd9ef179eebad55909a024b5
Kelvin <Kelzjr56@gmail.com>
2024-03-08 22:54:44 -0500

    Created Booking.h member declarations and methods

--- United_Flyers_Travel_Agency/Booking.h
+++ United_Flyers_Travel_Agency/Booking.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<string>
 
 /*
 	1: Allow customers to book a flight, can inherit logic from EstimateCost for ease of implementation.
@@ -7,7 +8,32 @@
 */
 
 class Booking {
+	public:
+		// Booking Constructors
+		Booking();
+		Booking(unsigned int c_flight_num, unsigned int c_customer_num, std::string c_airport_name, std::string c_gate_id);
 
+	private:
+		// Booking Private Members
+		unsigned int flight_number;
+		unsigned int customer_number;
+		std::string airport_name;
+		std::string gate_id;
 
+	public:
+		// Booking Setters
+		void setFlightNumber(unsigned int s_flight_num);
+		void setCustomerNumber(unsigned int s_customer_num);
+		void setAirportName(std::string s_airport_name);
+		void setGateId(std::string s_gate_id);
 
+		// Booking Getters
+		int getFlightNumber() const;
+		int getCustomerNumber() const;
+		std::string getAirportName() const;
+		std::string getGateId() const;
+		
+		// Destructor
+		~Booking();
+		
 };
