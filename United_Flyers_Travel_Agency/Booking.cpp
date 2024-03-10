a56741aabe60596204b74c7fd32df5a0ba4f58f3
Kelvin <Kelzjr56@gmail.com>
2024-03-09 22:21:43 -0500

    Added Booking Setters in Booking.cpp

--- United_Flyers_Travel_Agency/Booking.cpp
+++ United_Flyers_Travel_Agency/Booking.cpp
@@ -6,6 +6,38 @@
 
 #include "Booking.h"
 
+// Booking Constructors
+Booking::Booking() {
+	this->flight_number = 0;
+	this->customer_number = 0;
+	this->airport_name = "None";
+	this->gate_id = "None";
+}
+
+Booking::Booking(unsigned int c_flight_num, unsigned int c_customer_num, std::string c_airport_name, std::string c_gate_id) {
+	this->flight_number = c_flight_num;
+	this->customer_number = c_customer_num;
+	this->airport_name = c_airport_name;
+	this->gate_id = c_gate_id;
+}
+
+// Booking Setters
+void Booking::setFlightNumber(unsigned int s_flight_num) {
+	this->flight_number = s_flight_num;
+}
+
+void Booking::setCustomerNumber(unsigned int s_customer_num) {
+	this->customer_number = s_customer_num;
+}
+
+void Booking::setAirportName(std::string s_airport_name) {
+	this->airport_name = s_airport_name;
+}
+
+void Booking::setGateId(std::string s_gate_id) {
+	this->gate_id = s_gate_id;
+}
+
 
 
 
