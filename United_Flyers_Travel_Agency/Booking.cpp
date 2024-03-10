db73d63da4304d608586b7026734d3550a63d575
Kelvin <Kelzjr56@gmail.com>
2024-03-09 22:11:08 -0500

    Added Booking constructor definitions in Booking.cpp

--- United_Flyers_Travel_Agency/Booking.cpp
+++ United_Flyers_Travel_Agency/Booking.cpp
@@ -1,1 +1,11 @@
+/*
+	1: Allow customers to book a flight, can inherit logic from EstimateCost for ease of implementation.
+	2: Provide payment gateway, possibly from an external helper class.
+	3: Finalized payment details
+*/
+
+#include "Booking.h"
+
+
+
 
--- United_Flyers_Travel_Agency/Booking.h
+++ United_Flyers_Travel_Agency/Booking.h
@@ -1,11 +1,6 @@
 #pragma once
 #include<string>
 
-/*
-	1: Allow customers to book a flight, can inherit logic from EstimateCost for ease of implementation.
-	2: Provide payment gateway, possibly from an external helper class.
-	3: Finalized payment details
-*/
 
 class Booking {
 	public:
