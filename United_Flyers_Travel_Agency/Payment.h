e80a23b58c8a83e421972b489466234a65105148
Kelvin <Kelzjr56@gmail.com>
2024-03-13 23:44:54 -0400

    Corrected Payment declaration style and added constructors with destructor declarations

--- United_Flyers_Travel_Agency/Payment.h
+++ United_Flyers_Travel_Agency/Payment.h
@@ -9,21 +9,28 @@ class Payment{
 		unsigned int ccv_number;
 
 	public:
+		// Constructors
+		Payment();
+		Payment(unsigned int c_card_num, unsigned int c_exp_month, unsigned int c_card_year, unsigned int c_ccv_num);
+
 		// Constants
 		const int CARD_NUM_LENGTH = 16;
 		const int EXPIRATION_NUM_LENGTH = 2;
 		const int CCV_NUM_LENGTH = 3;
 
 		// Payment Setters
-		void setCardNumber(unsigned int card_num);
-		void setExpirationMonth(unsigned int card_month);
-		void setExpirationYear(unsigned int card_year);
-		void setCCVNumber(unsigned int ccv_num);
+		void setCardNumber(unsigned int s_card_num);
+		void setExpirationMonth(unsigned int s_card_month);
+		void setExpirationYear(unsigned int s_card_year);
+		void setCCVNumber(unsigned int s_ccv_num);
 
 		// Payment Getters
 		int getCardNumber() const;
 		int getExpirationMonth() const;
 		int getExpirationYear() const;
 		int getCcvNumber() const;
+
+		// Payment Destructor
+		~Payment();
 
 };
