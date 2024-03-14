ef1097c5ae20287515815404b0bd87c0ddb9acd6
Kelvin <Kelzjr56@gmail.com>
2024-03-13 23:53:50 -0400

    Created Payment.cpp and implemented Payment constructors

--- United_Flyers_Travel_Agency/Payment.cpp
+++ United_Flyers_Travel_Agency/Payment.cpp
@@ -1,1 +1,16 @@
 #include "Payment.h"
+
+// Payment Constructors
+Payment::Payment() {
+	this->card_number = 0000000000000000;
+	this->expiration_month = 01;
+	this->expiration_year = 23;
+	this->ccv_number = 000;
+}
+
+Payment::Payment(unsigned int c_card_num, unsigned int c_exp_month, unsigned int c_card_year, unsigned int c_ccv_num) {
+	this->card_number = c_card_num;
+	this->expiration_month = c_exp_month;
+	this->expiration_year = c_card_year;
+	this->ccv_number = c_ccv_num;
+}
